d3bc85987a5e11981c0b0ef18fef57e0976b2230
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-06 02:40:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5b20f9843b747925b890d6f7120d424ecb198262eba27337b2d7366918c8128a823d7d3aaee4c2c2a9431ec435162161d5dcbec4c75bf0c1230eb80d323a7b17'
