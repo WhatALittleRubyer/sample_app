a408d926a3add4c917042ab146c92776497d1bd2
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-18 22:05:44 -0400

    didn't mean to put explcit User in self.authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
   end
 
   def self.authenticate(email, password)
-    user = User.find_by_email(email)
+    user = find_by_email(email)
     return nil if user.nil?
     return user if user.has_password?(password)
   end
