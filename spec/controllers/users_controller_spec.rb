cd524dcc8f97d2b4fbd414ef8e43029b01fdef84
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-18 21:14:48 -0400

    Using user factory to test it shows the right user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  before(:each) do
+    @user = Factory(:user)
+  end
+
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
@@ -12,6 +16,11 @@ describe UsersController do
     it "should have 'Sign Up' in the title" do
       get 'new'
       response.should have_selector('title', :content => 'Sign Up')
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
     end
   end
 
