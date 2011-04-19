15c28a29df8ef6421c6a89d3d6715f73cfd09421
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-18 21:14:30 -0400

    added User factory

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+# by using the symbol ':user', we get FG to simulate the User model.
+Factory.define :user do |user|
+  user.name                   "Stink Bungford"
+  user.email                  "stink.bungford@foo.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
+  
