69792fc52a538ae4887b86ebd57bc651bb0b7332
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-20 18:04:10 -0400

    handle signup success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      #success
+      redirect_to @user
     else
       @title = "Sign Up"
       render :new
