7ddb9773a57af4ef424fe36e575218b3060e1d9f
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-20 17:58:42 -0400

    signup failures handled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #success
+    else
+      @title = "Sign Up"
+      render :new
+    end
+  end
 end
