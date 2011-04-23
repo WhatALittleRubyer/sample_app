8ced99b292572db865cd375d6b3beef93bd81798
What A Little Rubyer <mg.spam@hotmail.com>
2011-04-23 02:02:05 -0400

    sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
   end
 
   def self.authenticate_with_salt(id, cookie_salt)
-    user = find_by_id(d)
+    user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
 
