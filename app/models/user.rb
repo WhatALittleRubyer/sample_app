1d464c0f49ad245223b7ed4daa1339d0cc50f385
What A Little Rubyer <mg.spam@hotmail.com>
2011-05-14 00:29:51 -0400

    micropost user feed, and deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,11 @@ class User < ActiveRecord::Base
     (user && user.salt == cookie_salt) ? user : nil
   end
 
+  def feed
+    # Preliminary!
+    Micropost.where("user_id = ?", id)
+  end
+
   private
     def encrypt_password
       self.salt = make_salt if new_record?
